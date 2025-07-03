521e5390336f125b1152090a97b67521b0e49680
henrique_menel <henriqueworks68@gmail.com>
2025-07-03 16:52:50 -0300

    2.1 - adicionando class pedidos

---
+++ src/main/java/dao/PedidosDAO.java
@@ -0,0 +1,26 @@
+package dao;
+
+import model.Conexao;
+import model.Pedidos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PedidosDAO {
+    public void inserir(Pedidos pedidos){
+        String sql = "INSERT INTO pedidos (cliente, data_pedido, total) VALUES (?,?,?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, pedidos.getCliente());
+            stmt.setString(2, pedidos.getDataPedido());
+            stmt.setDouble(3, pedidos.getTotal());
+
+            System.out.println("Pedido inserido com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
